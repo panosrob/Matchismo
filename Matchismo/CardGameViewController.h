f08c1e0039b2f9246f22d5ea188fc71baa263a32
Panagiotis Rompolas <panosrob@Panagiotiss-MacBook-Pro.local>
2013-01-29 00:16:19 +0200

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Panagiotis Rompolas on 1/29/13.
+//  Copyright (c) 2013 Panagiotis Rompolas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
